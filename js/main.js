74a694970b0a9b46ec42008b55dd85d1a39ea8d2
Jeff <80537736+root-jeff@users.noreply.github.com>
2022-03-20 22:00:06 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function sidicequesi(){
+    alert("Sabía que dirías que sí, no tenías otra opción, de todas maneras Te amo <3");
+}
+
+function quetoquelboton(){
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    newWidth = (Math.random() * width);
+    newHeight = (Math.random() * height);
+
+    document.getElementById("btnNo").style.position = "absolute";
+    document.getElementById("btnNo").style.left = newWidth + "px";
+    document.getElementById("btnNo").style.top = newHeight + "px";
+}
